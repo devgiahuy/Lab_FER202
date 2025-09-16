b1b431f0194ccbbfbe0b18d808e3d2ada3f6aef6
NGOGIAHUY24 <huyngse183274@fpt.edu.vn>
2025-09-16 11:51:22 +0700

    do: done navbar login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,27 @@
-// import { NavbarDemo } from "../components"
-// import BodyContent from "../components/shared/BodyContent"
+import { NavbarDemo } from "../components"
+import BodyContent from "../components/shared/BodyContent"
 import LoginDemo from "../components/shared/LoginDemo"
 
-// function App() {
-//     return (
-//         <div>
-//             {/* <NavbarDemo />
-//             <BodyContent /> */}
-//             <LoginDemo></LoginDemo>
-//         </div>
-//     )
-// }
-
-
-export default function App() {
-
-  return (
-   <div>
-        <LoginDemo></LoginDemo>
-    </div>
-
-
-  );
+function App() {
+    return (
+        <div>
+            <NavbarDemo />
+            <BodyContent />
+            <LoginDemo></LoginDemo>
+        </div>
+    )
 }
 
-// export default App
+
+// export default function App() {
+
+//   return (
+//    <div>
+//         <LoginDemo></LoginDemo>
+//     </div>
+
+
+//   );
+// }
+
+export default App
